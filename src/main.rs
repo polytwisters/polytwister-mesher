a641616e340a11ca64af25285629e57d360e1dae
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-12-26 08:14:19 -0800

    Move into subcommand

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{fs::File, io::Read};
 use serde::Deserialize;
 extern crate nalgebra as na;
 use na::{Point3, Vector4};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 mod cylinder_intersections;
 mod pipe_section;
@@ -20,31 +20,44 @@ mod ring;
 mod polytwister;
 use crate::polytwister::Polytwister;
 
-
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Input JSON file describing the geometry of the polytwister.
-    input_json: PathBuf,
-
-    /// Output PLY mesh file.
-    output_ply: PathBuf,
-
     config: Option<PathBuf>,
+    
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    CrossSection {
+        /// Input JSON file describing the geometry of the polytwister.
+        input_json: PathBuf,
+
+        /// W cross section
+        w: f64,
+
+        /// Output PLY mesh file.
+        output_ply: PathBuf,
+    }
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    let mut string = String::new();
-    let mut file = File::open(args.input_json)?;
-    file.read_to_string(&mut string)?;
+    match &args.command {
+        Commands::CrossSection { input_json, w, output_ply } => {
+            let mut string = String::new();
+            let mut file = File::open(input_json)?;
+            file.read_to_string(&mut string)?;
 
-    let polytwister: Polytwister = serde_json::from_str(&string)?;
-    let w = 0.1;
-    let mesh = polytwister.as_mesh(w);
+            let polytwister: Polytwister = serde_json::from_str(&string)?;
+            let mesh = polytwister.as_mesh(*w);
 
-    let mut buffer = File::create(args.output_ply)?;
-    mesh.write_ply(&mut buffer)?;
-    Ok(())
+            let mut buffer = File::create(output_ply)?;
+            mesh.write_ply(&mut buffer)?;
+            Ok(())
+        }
+    }
 }
