51cd6603610e970761d72fd6cadcea9e343fa679
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-12-26 09:33:48 -0800

    Add animation code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 extern crate approx;
 
 use core::f64;
+use std::fs;
+use std::error::Error;
+use std::path;
 use std::path::PathBuf;
 use std::{fs::File, io::Read};
 use serde::Deserialize;
@@ -21,6 +24,7 @@ mod polytwister;
 mod config;
 use crate::config::Config;
 use crate::polytwister::Polytwister;
+use crate::utils::linspace;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -33,8 +37,19 @@ struct Args {
     command: Commands,
 }
 
+
 #[derive(Subcommand)]
 enum Commands {
+    Animation {
+        /// Input JSON file describing the geometry of the polytwister.
+        input_json: PathBuf,
+
+        /// Number of animation frames.
+        frames: usize,
+
+        /// Output directory.
+        output_dir: PathBuf,
+    },
     Section {
         /// Input JSON file describing the geometry of the polytwister.
         input_json: PathBuf,
@@ -47,7 +62,7 @@ enum Commands {
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let config: Config = if let Some(config_path) = args.config {
@@ -66,12 +81,33 @@ fn main() -> std::io::Result<()> {
             let mut string = String::new();
             let mut file = File::open(input_json)?;
             file.read_to_string(&mut string)?;
-
             let polytwister: Polytwister = serde_json::from_str(&string)?;
+
             let mesh = polytwister.as_mesh(*w, &config);
 
             let mut buffer = File::create(output_ply)?;
             mesh.write_ply(&mut buffer)?;
+            Ok(())
+        },
+        Commands::Animation { input_json, output_dir, frames } => {
+            let config: Config = Default::default();
+
+            let mut string = String::new();
+            let mut file = File::open(input_json)?;
+            file.read_to_string(&mut string)?;
+            let polytwister: Polytwister = serde_json::from_str(&string)?;
+
+            std::fs::create_dir(output_dir)?;
+
+            for (i, w) in linspace(-1.0, 1.0, *frames).into_iter().enumerate() {
+                eprintln!("frame {i}, w = {w}");
+                let file_name = format!("section_{i:04}.ply");
+                let output_ply = output_dir.join(file_name);
+                let mesh = polytwister.as_mesh(w, &config);
+                let mut buffer = File::create(output_ply)?;
+                mesh.write_ply(&mut buffer)?;
+            }
+
             Ok(())
         }
     }
