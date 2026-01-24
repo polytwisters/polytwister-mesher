a57efafcc8449b9fc5751e0d0deeb91d251026ab
Nathan Ho <nathan.waikin.ho@gmail.com>
2026-01-24 00:17:05 -0800

    Add end-to-end recipe support

---
+++ blender/run_everything_section.py
@@ -0,0 +1,60 @@
+import subprocess
+import argparse
+import json
+import pathlib
+
+import common
+import meshes_to_blend
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("out_dir", type=str)
+    args = parser.parse_args()
+
+    input_file = pathlib.Path(args.input_file)
+    out_dir = pathlib.Path(args.out_dir)
+
+    with open(input_file) as f:
+        config_root = json.load(f)
+
+    out_dir.mkdir(parents=True)
+    
+    polytwister = config_root["polytwister"]
+    w = config_root["w"]
+
+    mesher_config_file = out_dir / "mesher_config.json"
+    with open(mesher_config_file, "w") as f:
+        json.dump(config_root["mesher_config"], f)
+    
+    mesh_dir = out_dir / "meshes"
+    blend = out_dir / "section.blend"
+
+    subprocess.run([
+        "cargo", "run", "--release",
+        "--",
+        "--config", mesher_config_file,
+        "section",
+        polytwister,
+        "-w", str(w),
+        "--split", mesh_dir,
+    ], check=True)
+
+    meshes_to_blend.export_mesh_directory_as_blend(
+        mesh_dir,
+        blend,
+        config=config_root["blender_config"]
+    )
+
+    subprocess.run([
+        common.BLENDER,
+        "--background",
+        blend,
+        "--render-output",
+        out_dir / "render",
+        "--render-format", "PNG",
+        "--render-frame", "1",
+    ])
+
+if __name__ == "__main__":
+    main()
