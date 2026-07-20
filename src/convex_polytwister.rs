0a1df243dc9b5dd6c8dddb67ca6495fa0856add6
Nathan Ho <nathan.waikin.ho@gmail.com>
2026-07-19 18:50:23 -0700

    Improve test

--- src/convex_polytwister.rs
+++ src/convex_polytwister.rs
@@ -251,7 +251,7 @@ mod test {
     fn dyster() -> ConvexPolytwister {
         ConvexPolytwister::new(vec![
             Log::new(C2::from_components(1.0, 0.0, 0.2, -0.1)),
-            Log::new(C2::from_components(0.0, 0.0, 1.0, 0.0)),
+            Log::new(C2::from_components(0.1, 0.2, 1.0, 0.0)),
             Log::new(C2::from_components(0.0, 1.0, 1.0, 0.0)),
         ])
     }
@@ -269,11 +269,11 @@ mod test {
     #[test]
     fn test_cross_section_basic() {
         let dyster = dyster();
-        let rings = dyster.rings.clone();
-        assert_eq!(rings.len(), 2);
-        assert!(dyster.contains(&Fiber::zero()));
-        assert!(dyster.contains(&rings[0]));
-        assert!(dyster.contains(&rings[1]));
+        let meshes = dyster.as_meshes(0.3, &Config::default());
+        assert!(meshes.ring_meshes.len() > 0);
+        assert!(meshes.strip_meshes.len() > 0);
+        assert!(meshes.twister_meshes_orbit_1.len() > 0);
+        assert!(meshes.twister_meshes_orbit_2.len() == 0);
     }
 
     /// Issue #11, fails because ConvexPolytwister does not correctly handle L(0, 1).
