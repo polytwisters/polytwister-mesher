e085b612064f29188ee7897763f119e7d8f5fad9
Nathan Ho <nathan.waikin.ho@gmail.com>
2026-07-21 18:09:24 -0700

    Finish

--- tools/random_convex_polytwister.py
+++ tools/random_convex_polytwister.py
@@ -7,14 +7,22 @@ def similarity(a, b):
     return np.abs(np.dot(a, b.conj())) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
-def random_vec4(rng):
-    r4 = rng.standard_normal(4)
-    r4 = r4 / np.linalg.norm(r4)
-    radius = rng.triangular(0.8, 1.0, 1.2)
-    r4 = r4 * radius
-    r4 = np.round(r4, 1)
-    c2 = r4[0::2] + 1j * r4[1::2]
-    return (c2, r4)
+def random_c2(rng):
+    result = rng.standard_normal(4)
+    result = result / np.linalg.norm(result)
+    radius = rng.triangular(0.7, 1.0, 1.3)
+    result = result * radius
+    result = np.round(result, 1)
+    return r4_to_c2(result)
+
+def r4_to_c2(r4):
+    return r4[0::2] + 1j * r4[1::2]
+
+def c2_to_r4(c2):
+    result = np.zeros(len(r4) * 2)
+    result[0::2] = c2.real()
+    result[1::2] = c2.imag()
+    return result
 
 
 def random_convex_polytwister(n: int):
@@ -22,15 +30,13 @@ def random_convex_polytwister(n: int):
     logs_real = []
     rng = np.random.default_rng(0)
     for i in range(n):
-        c2, r4 = random_vec4(rng)
-        for __ in range(1000):
-            if min([similarity(c2, log_c2) for log_c2 in logs_c2], default=0.0) < 0.9:
+        c2 = random_c2(rng)
+        for __ in range(100):
+            max_similarity = max([similarity(c2, log_c2) for log_c2 in logs_c2], default=0.0)
+            if max_similarity < 0.9:
                 break
-            c2, r4 = random_vec4(rng)
-        else:
-            print("warning not found")
         logs_c2.append(c2)
-        logs_real.append(r4.tolist())
+        logs_real.append(c2_to_r4(c2).tolist())
     return {"logs": logs_real}
 
 
