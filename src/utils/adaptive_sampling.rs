e199b8556bb3c18e2dee79d9e231a9885a897f01
Nathan Ho <nathan.waikin.ho@gmail.com>
2026-07-22 09:41:04 -0700

    Improve docs and comments

--- src/utils/adaptive_sampling.rs
+++ src/utils/adaptive_sampling.rs
@@ -1,3 +1,14 @@
+/**
+ * Given a distance function d(t1, t2) and an interval on the real line, produce a list of t values
+ * in that interval such that d(t1, t2) is almost always less than a given maximum distance. The
+ * list of t values may be linear (interval is closed and the output contains both endpoints), or
+ * circular (interval is half-open).
+ * 
+ * This works by first evenly spacing initial_num_points in the interval, and for each pair of
+ * consecutive t1, t2, if d(t1, t2) > max_distance then subdivide the interval [t1, t2] into
+ * ceil(max_distance / d(t1, t2)) segments. This does not formally guarantee that the maximum
+ * distance is adhered to but in practice it basically always works.
+ */
 pub fn adaptive_sample<F : Fn (f64, f64) -> f64>(
     distance_func: F,
     max_distance: f64,
@@ -6,30 +17,34 @@ pub fn adaptive_sample<F : Fn (f64, f64) -> f64>(
     max: f64,
     circular: bool
 ) -> Vec<f64> {
-    let initial: Vec<_> = (0..initial_num_points).map(|i| {
+    // Start with an evenly spaced number of points. In the circular case, they are evenly spaced
+    // including min but excluding max. In the linear case, they are evenly spaced including both,
+    // hence the conditional.
+    let evenly_spaced: Vec<_> = (0..initial_num_points).map(|i| {
         let t = i as f64 / (if circular { initial_num_points } else { initial_num_points - 1 }) as f64;
         min + (max - min) * t
     }).collect();
 
     // Walk through each line segment connecting pairs of points and check distances.
-
-    // Rough heuristic, assume the number of new points needed is about twice.
-    let mut result: Vec<f64> = Vec::with_capacity(initial.len() * 2);
-    for i1 in 0usize..initial.len() {
-        let x1 = initial[i1];
-        // Always keep the first point in the segment.
+    // Heuristic to reduce unnecessary reallocations: assume the final number of points needed is
+    // about twice the initial num points.
+    let mut result: Vec<f64> = Vec::with_capacity(evenly_spaced.len() * 2);
+    for i1 in 0usize..evenly_spaced.len() {
+        // Always add the point from the original.
+        let x1 = evenly_spaced[i1];
         result.push(x1);
 
         let mut i2 = i1 + 1;
-        if !circular && i2 >= initial.len() {
+        // In the linear case, the final segment is ignored.
+        if !circular && i2 >= evenly_spaced.len() {
             break;
         }
-        i2 = i2.rem_euclid(initial.len());
+        i2 = i2.rem_euclid(evenly_spaced.len());
 
-        let x2 = initial[i2];
-        let d = distance_func(x1, x2);
+        let x2 = evenly_spaced[i2];
         // If the distance between successive points is larger than the minimum, subdivide it
         // into smaller segments.
+        let d = distance_func(x1, x2);
         if d > max_distance {
             let subdivisions = (d / max_distance).ceil() as usize;
             // Start with 1 here, as we already added theta1.
